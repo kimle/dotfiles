434c1f44c938429720490f24bc59fc8b5ecd3af9
Kim Le <kim@kimle.dev>
2026-08-29 17:44:15 +0300

    forgit: size delta to the preview pane

--- chezmoi/dot_config/fish/config.fish
+++ chezmoi/dot_config/fish/config.fish
@@ -1,6 +1,7 @@
 set TERM xterm-256color
 set EDITOR vim
 set EZA_CONFIG_DIR $HOME/.config/eza
+set -gx FORGIT_PREVIEW_PAGER 'delta --width=$FZF_PREVIEW_COLUMNS'
 if type -q atuin
     atuin init fish | source
 end
